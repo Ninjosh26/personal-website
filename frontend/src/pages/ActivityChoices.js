fc59bf6dc58cc8ad00fd7397acb010e49749c3fb
Josh Stafford <jdavstaff@tamu.edu>
2024-12-12 19:14:10 -0800

    Update taskbar label

--- frontend/src/pages/ActivityChoices.js
+++ frontend/src/pages/ActivityChoices.js
@@ -1,9 +1,14 @@
+import { useEffect } from "react";
 import { ActivityOptions } from "../components";
 import useActivities from "../hooks/useActivities";
 
 export default function ActivityChoices() {
   const { movies, tvShows, games, addActivity, deleteActivities } =
     useActivities();
+
+  useEffect(() => {
+    document.title = "Kate's Website - Activity Choices";
+  }, []);
 
   return (
     <div style={{ paddingLeft: "20px", paddingRight: "20px" }}>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    document.title = "Kate's Website - Home Page";
+  }, []);
+
   return (
     <div style={{ paddingLeft: "20px", paddingRight: "20px" }}>
       <h1>Home Page</h1>
