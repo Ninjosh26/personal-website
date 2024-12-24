7dc7250991577932663ad0e39aa5a4b8bb83d4e9
Josh Stafford <jdavstaff@tamu.edu>
2024-12-23 21:10:07 -0600

    Increase font size of table

--- frontend/src/components/ActivityTable.js
+++ frontend/src/components/ActivityTable.js
@@ -22,9 +22,10 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
+    fontSize: 18,
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    fontSize: 18,
   },
 }));
 
